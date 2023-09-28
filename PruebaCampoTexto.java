733b68d2f867fa31e18de1450a1f6a4436fc66f6
iDaniDots <perzonhominid@gmail.com>
2023-09-28 10:08:29 -0600

    Código 3

---
+++ PruebaCampoTexto.java
@@ -0,0 +1,11 @@
+// Fig. 12.10: PruebaCampoTexto.java
+// Prueba de CampoTextoMarco.
+import javax.swing.JFrame;
+public class PruebaCampoTexto {
+    public static void main(String[] args) { 
+        CampoTextoMarco campoTextoMarco = new CampoTextoMarco(); 
+        campoTextoMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        campoTextoMarco.setSize(350, 100); 
+        campoTextoMarco.setVisible(true);
+    } 
+} // fin de la clase PruebaCampoTexto
